a6e9bbdc491cd8dd550bd1744b64b9e7fba74bb6
Grigory <mail@grigor.io>
2024-10-15 00:04:59 +0300

    Run db prepare always

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 rm -f /custom_table_demo/tmp/pids/server.pid
 
 # If running the rails server then create or migrate existing database
-if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
-  ./bin/rails db:prepare
-fi
+# if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
+./bin/rails db:prepare
+# fi
 
 exec "${@}"
